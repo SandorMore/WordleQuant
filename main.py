91c67e9f26e83d89be98d505e8cc2803e16c65e5
SandorMore <msaso0725@gmail.com>
2026-04-17 15:24:03 +0200

    starter screen added

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def main()
+import pygame as pg
 
-
+def main():
+    pg.init()
+    screen = pg.display.set_mode((1280, 720))
+    clock = pg.time.Clock()
+    while True:
+       screen.fill(color = "purple")
+       pg.display.flip()
+       clock.tick(60)
 if __name__ == "__main__":
  main()
