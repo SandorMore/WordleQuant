48169e6b8b71a3fb2d9192226db6d4acdf055c7c
Saso <msaso0725@gmail.com>
2026-04-21 13:18:04 +0200

    added letter slots

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 import pygame as pg
-from utilities.model import Word 
+from utilities.model import Word, Slot 
 from utilities.pygameutil import Reader
 from utilities.model import wordFrequency
+
 reader = Reader("assets/words.txt")
 word = Word(5)
+slotList : list = []
+def DrawSlots(s:pg.display):
+    
+    left, top = 100, 70
+    initialLeft = 100
 
+    for i in range(6):
+        for i in range(5):
+            pg.draw.rect(s, "black", [left, top, 100, 100], 3)
+            slotList.append(Slot(left,top,100,100))
+            left += 130
+
+        left = initialLeft
+        top += 110
+       
+       
 def main():
     pg.init()
-    screen = pg.display.set_mode((1280, 720))
+    screen = pg.display.set_mode((1280, 860))
     clock = pg.time.Clock()
+
     while True:
-       screen.fill(color = "green")
-       
-       for event in pg.event.get():
+        screen.fill(color = "gray")
+
+        DrawSlots(s=screen)
+
+        for event in pg.event.get():
           if event.type == pg.QUIT:
             pg.quit()
             raise SystemExit
        
-       pg.display.flip()
+        pg.display.flip()
     
-       clock.tick(60)
+        clock.tick(60)
 
 if __name__ == "__main__":    
     reader.printAllWords()
--- utilities/model.py
+++ utilities/model.py
@@ -1,4 +1,7 @@
+import pygame as pg
+
 wordFrequency : dict = {}
+
 class Word:
     length:int
     word:str
@@ -11,3 +14,10 @@ class Word:
         for i in range(0, 5):
             if(guess[i] == self.word):
                 pass
+            
+class Slot:
+    rect:pg.Rect
+    letter:chr
+
+    def __init__(self, left:int, top:int, width:int, height:int):
+        self.rect = pg.Rect(left,top,width,height)
